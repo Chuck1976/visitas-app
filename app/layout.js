065e283ab570873a4519746a8a28c59940be9121
Alessandro Antonelli <alejandro.antonelli76@gmail.com>
2026-07-13 11:39:06 +0200

    Fix mobile viewport layout

--- app/layout.js
+++ app/layout.js
@@ -24,6 +24,7 @@ export const metadata = {
 export const viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
   viewportFit: "cover",
 };
 
@@ -33,12 +34,6 @@ export default function RootLayout({ children }) {
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable}`}
     >
-      <head>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, viewport-fit=cover"
-        />
-      </head>
       <body>{children}</body>
     </html>
   );
